0280587bccd7e7a505ef05d45adcebcf083cf9a4
Prabhu <prabhu.raja@rci.rogers.com>
2020-05-09 08:42:30 -0400

    1 - module - Used builtin path module

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 const mlog = require('./logger');
+const path = require('path');
+
+const parseFileName = path.parse(__filename);
+const parseDirName = path.parse(__dirname);
 mlog(' is flying');
+mlog(parseFileName);
+mlog(parseDirName);
