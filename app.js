855ae793feb007525cf4c2615be6891565e5db7a
Prabhu <prabhu.raja@rci.rogers.com>
2020-05-09 08:51:41 -0400

    2 - module - Used builtin os module

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const mlog = require('./logger');
 const path = require('path');
+const os = require('os');
 
 const parseFileName = path.parse(__filename);
 const parseDirName = path.parse(__dirname);
 mlog(' is flying');
+//
 mlog(parseFileName);
 mlog(parseDirName);
+//
+console.log(`You have ${os.freemem}/${os.totalmem}`);
