ea87dfa32fdc2476914d31e46d32d76dc856426e
Prabhu <prabhu.raja@rci.rogers.com>
2020-05-13 08:46:51 -0400

    3-Express - 8 - Handling HTTP Post Request

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const courses = [
   { id:1, name: 'course1' },
   { id:2, name: 'course2' },
@@ -15,7 +17,16 @@ app.get('/', (req, res) => {
 
 app.get('/api/courses', (req, res) => {
   res.send(courses);
-})
+});
+
+app.post('/api/courses', (req, res) => {
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  }
+  courses.push(course);
+  res.send(course);
+});
 
 app.get('/api/courses/:id', (req, res) => {
   const course = courses.find(val => val.id === parseInt(req.params.id));
@@ -23,7 +34,7 @@ app.get('/api/courses/:id', (req, res) => {
     res.status(404).send('Course with given id was not found😿');
   }
   res.send(course);
-})
+});
 
 const port = process.env.PORT || 5000;
 // * In MAC we can set port by adding command 'export PORT=5000'
