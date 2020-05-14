fc0d2fc6461b7fa371684009293be6c1e316b51a
prabhu-raja <prabhuraja@gmail.com>
2020-05-13 22:13:45 -0400

    3-Express - 9 - Basic Post inpit validation

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ app.get('/api/courses', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
+  if(!req.body.name || req.body.name.length < 3) {
+    // 400 Bad Request
+    res.status(400).send('Name is required and should be minimun 3 characters');
+  }
   const course = {
     id: courses.length + 1,
     name: req.body.name
