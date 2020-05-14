599e8cdd3127f80b913f15e9e402d9361f959e03
prabhu-raja <prabhuraja@gmail.com>
2020-05-13 22:42:24 -0400

    3-Express - 10 - Handled Joi  validation

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
@@ -20,9 +21,13 @@ app.get('/api/courses', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
-  if(!req.body.name || req.body.name.length < 3) {
+  const schema = {
+    name: Joi.string().min(3).required()
+  }
+  const result = Joi.validate(req.body, schema);
+  if(result.error) {
     // 400 Bad Request
-    res.status(400).send('Name is required and should be minimun 3 characters');
+    res.status(400).send(result.error.details[0].message);
   }
   const course = {
     id: courses.length + 1,
