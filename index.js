6cc66f58857fc6ca7b54f8f2be7f22449d6a97b3
prabhu-raja <prabhuraja@gmail.com>
2020-05-15 07:31:35 -0400

    4-Adv-Express - 1 - Simple middleware check

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log('🚕Logging....🚕');
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('🚓Authenticating....🚓');
+  next();
+});
+
 const courses = [
   { id:1, name: 'course1' },
   { id:2, name: 'course2' },
