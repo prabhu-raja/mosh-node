1982eaa4bafca937eb28db18458e2896d057c6e2
prabhu-raja <prabhuraja@gmail.com>
2020-05-16 08:13:52 -0400

    4-Adv-Express - 6 - Get Environment type

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const logger = require('./init-logger');
 const express = require('express');
 const app = express();
 
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+console.log(`app: ${app.get('env')}`);
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
